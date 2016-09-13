d5fceb88fecdf84c00cc2e265b2e4920c710ec80
Sunnie Grace McCalla <grace.mccalla@gmail.com>
2016-09-13 10:19:30 -0500

    Modified step4

--- remove_colons.sh
+++ remove_colons.sh
@@ -3,19 +3,4 @@
 #
 #sed -i 's/\r//' step1_GAIM01_14jul2016.sh
 #chmod -R 777 .
-#cd /home/smccalla/htcondor/$(the_analysis_name)/condor_out/results/
-#sed 's/:/_/g' -i /home/smccalla/htcondor/$(the_analysis_name)/data/fasta_files/group_*.out
-#cat group*.out > $(the_analysis_name).blastout
-#cd ..
-#mkdir blast_summary && cp -r /home/smccalla/htcondor/$(the_analysis_name)/data/fasta_files/$(the_analysis_name).blastout .
-
-#while read -r f; do mv "$f" "${f//:/_}"; done <files.txt
-
-#rename 's/:/_/g' *
-
-#Run outside of condor:
-cd /home/smccalla/htcondor/GAIM_12/condor_out/results/
-sed 's/:/_/g' -i /home/smccalla/htcondor/GAIM_12/data/fasta_files/group_*.out
-cat group*.out > GAIM_12.blastout
-cd ..
-mkdir blast_summary && cp -r /home/smccalla/htcondor/GAIM_12/data/fasta_files/GAIM_12.blastout .
+sed s/://g /home/smccalla/htcondor/GAIM_01/data/fasta_files/group_*/data/fasta_files/group_*.out
